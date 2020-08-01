634c5d9d0698ee9551e3a852b6994bbbf5565eff
Roger Aspelin <roger.aspelin@hotmail.se>
2020-08-01 02:49:56 +0200

    Setup of migration and part of modell. Also factory is created.

--- app/Club.php
+++ app/Club.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Club extends Model
 {
-    //
+    //TODO:
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'country',
+        'user_id',
+        'owner_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+
+    }
 }
