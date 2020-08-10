a75045aad020e3be4e8e0a9383ff8dad2b6cbe15
Roger Aspelin <roger.aspelin@hotmail.se>
2020-08-10 17:48:20 +0200

    Create UserTest.php
    User test

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function check_if_user_is_admin_or_not_admin()
+    {
+        factory(User::class)
+            ->create([
+                'admin_since' => null,
+                ]);
+
+        $user = User::find(1);
+        $this->assertNotTrue($user->isAdmin());
+
+        $user->admin_since = now();
+        $user->save();
+        $this->assertTrue($user->isAdmin());
+
+    }
+}
