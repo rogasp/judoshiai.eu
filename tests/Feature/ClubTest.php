32aeec5b80cec7cb0eba2a1c4084677cbff6cd8e
Roger Aspelin <roger.aspelin@hotmail.se>
2020-08-11 23:21:44 +0200

    Update ClubTest.php
    Tests for clubs added

--- tests/Feature/ClubTest.php
+++ tests/Feature/ClubTest.php
@@ -72,9 +72,7 @@ class ClubTest extends TestCase
     public function check_if_authenticated_user_is_owner()
     {
         factory(Club::class)
-            ->create([
-                'approved_at' => null,
-            ]);
+            ->create();
 
         $club = Club::find(1);
 
@@ -84,4 +82,144 @@ class ClubTest extends TestCase
         $this->assertTrue($club->is_owner());
 
     }
+
+    /**
+     * @test
+     */
+    public function check_if_authenticated_user_is_admin()
+    {
+        factory(Club::class)
+            ->create();
+
+        $club = Club::find(1);
+
+        $user = factory(User::class)
+            ->create();
+
+        $club->administrators()->attach($user->id);
+
+        $this->actingAs($user);
+
+        $this->assertTrue($club->is_admin());
+    }
+
+    /**
+     * @test
+     */
+    public function do_i_get_clubs_im_involved_with_as_owner()
+    {
+        $user = factory(User::class)
+            ->create();
+
+        $club = factory(Club::class)
+            ->create([
+                'owner_id' => $user->id,
+            ]);
+
+        $this->actingAs($user);
+
+        $clubs = Club::involved()->get();
+
+        $this->assertEquals($club->id,$clubs->first()->id);
+    }
+
+    /**
+     * @test
+     */
+    public function do_i_get_clubs_im_involved_with_as_administrator()
+    {
+        $user = factory(User::class)
+            ->create();
+
+        $club = factory(Club::class)
+            ->create();
+
+        $this->actingAs($user);
+
+        $club->administrators()->attach($user->id);
+
+        $clubs = Club::involved()->get();
+
+        $this->assertEquals($club->id,$clubs->first()->id);
+    }
+
+    /**
+     * @test
+     */
+    public function do_i_get_clubs_im_involved_with_as_owner_and_administrator_to_different_clubs()
+    {
+        $user = factory(User::class)
+            ->create();
+
+        $club1 = factory(Club::class)
+            ->create();
+
+        $club2 = factory(Club::class)
+            ->create([
+                'owner_id' => $user->id
+            ]);
+
+        $this->actingAs($user);
+
+        $club1->administrators()->attach($user->id);
+
+        $clubs = Club::involved()->get();
+
+        $this->assertEquals(2, $clubs->count());
+
+    }
+
+    /**
+     * @test
+     */
+    public function do_i_get_clubs_im_involved_with_as_owner_and_administrator_to_same_club()
+    {
+        $user = factory(User::class)
+            ->create();
+
+        $club1 = factory(Club::class)
+            ->create();
+
+        $club2 = factory(Club::class)
+            ->create([
+                'owner_id' => $user->id
+            ]);
+
+        $this->actingAs($user);
+
+        $club2->administrators()->attach($user->id);
+
+        $clubs = Club::involved()->get();
+
+        $this->assertEquals(1, $clubs->count());
+        $this->assertEquals($club2->id, $clubs->first()->id);
+    }
+
+    /**
+     * @test
+     */
+    public function should_not_get_clubs_when_not_involved_as_owner_or_administrator()
+    {
+        $user = factory(User::class)
+            ->create();
+
+        $club1 = factory(Club::class)
+            ->create();
+
+        $club2 = factory(Club::class)
+            ->create();
+
+        $club2->administrators()->attach($user->id);
+
+        $user = factory(User::class)
+            ->create();
+
+        $this->actingAs($user);
+
+        $clubs = Club::involved()->get();
+
+        $this->assertEquals(0, $clubs->count());
+    }
+
+
 }
